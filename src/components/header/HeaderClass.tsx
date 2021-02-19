ddf0ec78c909e68b9f1b3c12d922f2d5311a70f8
tangben <240193690@qq.com>
2021-02-20 00:19:16 +0800

    .

---
+++ src/components/header/HeaderClass.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import styles from './header.module.css'
+import logo from '../../logo.svg'
+import { Layout, Typography, Input, Dropdown, Menu, Button } from 'antd'
+import { withRouter, RouteComponentProps } from 'react-router-dom'
+import store from '../../redux/store'
+
+
+class HeaderCom extends React.Component<RouteComponentProps> {
+
+  constructor(props) {
+    super(props)
+    const storeState = store.getState()
+    this.state = {
+      nihao: storeState.nihao,
+    }
+    store.subscribe(() => {
+      const storeState = store.getState()
+      this.setState({
+        nihao: storeState.nihao,
+      })
+    })
+  }
+  toLogin() {
+    const history = this.props.history
+    history.push({ pathname: '/login', state: { a: 2 } })
+  }
+  toReg() {
+    const history = this.props.history
+    history.push({ pathname: '/reg', state: { a: 2 } })
+  }
+  change() {
+    console.log('------');
+
+    const action = {
+      type: 'aaa',
+      value: '777'
+    }
+    store.dispatch(action)
+  }
+  render() {
+    return <>
+      <div className={styles['app-header']}>
+        {/* top-header */}
+        <div className={styles['top-header']}>
+          <div className={styles['inner']}
+            onClick={() => { this.change() }}>
+            <Typography.Text>{(this.state as any).nihao}89</Typography.Text>
+            <Dropdown.Button style={{ marginLeft: 15 }} overlay={
+              <Menu>
+                <Menu.Item>中文</Menu.Item>
+                <Menu.Item>英语</Menu.Item>
+              </Menu>
+            }>
+              语言
+          </Dropdown.Button>
+            <Button.Group className={styles['btn-group']}>
+              <Button onClick={() => { this.toLogin() }}>登录</Button>
+              <Button onClick={() => { this.toReg() }}>注册</Button>
+            </Button.Group>
+          </div>
+        </div>
+        <Layout.Header className={styles['main-header']}>
+          <img src={logo} alt="" className={styles['app-logo']} />
+          <Typography.Title className={styles['title']} level={3}>鹰眼</Typography.Title>
+          <Input.Search placeholder={'请输入...'} className={styles['sear']} />
+        </Layout.Header>
+        <Menu className={styles['main-menu']} mode={'horizontal'}>
+          <Menu.Item key={1}>国际旅游</Menu.Item>
+          <Menu.Item key={2}>同城旅游</Menu.Item>
+          <Menu.Item key={3}>单人游</Menu.Item>
+          <Menu.Item key={4}>组团游</Menu.Item>
+          <Menu.Item key={5}>峡谷游</Menu.Item>
+          <Menu.Item key={6}>郊游</Menu.Item>
+        </Menu>
+      </div>
+    </>
+  }
+}
+
+export const Header = withRouter(HeaderCom)
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -22,7 +22,7 @@ export class Home extends React.Component {
   render() {
     return (
       <div className={styles.app}>
-        <Header change={this.nihao} />
+        <Header />
         <div className={styles['page-content']}>
           <Row>
             <Col span={6}>
